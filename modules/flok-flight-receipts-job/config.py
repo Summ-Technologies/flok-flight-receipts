096cee4d94298fa0748770d6fd9888f94378f0fd
Jared Hanson <jred0011@gmail.com>
2022-01-05 09:12:08 -0800

    removed unused modules

--- modules/flok-flight-receipts-job/config.py
+++ modules/flok-flight-receipts-job/config.py
@@ -3,13 +3,6 @@ import os
 # SQLALCHEMY
 SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
 
-# RMQ connection config
-RMQ_USER = os.environ["RMQ_USER"]
-RMQ_PASSWORD = os.environ["RMQ_PASSWORD"]
-RMQ_HOST = os.environ["RMQ_HOST"]
-RMQ_PORT = os.environ["RMQ_PORT"]
-RMQ_SECURE = os.environ.get("RMQ_SECURE")
-
 # Google Credentials
 GOOGLE_SERVICE_ACC_FILE = os.environ["GOOGLE_SERVICE_ACC_FILE"]
 GOOGLE_SERVICE_WORKER_USER_ID = os.environ["GOOGLE_SERVICE_WORKER_USER_ID"]
